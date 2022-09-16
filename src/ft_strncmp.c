d5d3d65af3a9cca880633440421ae88a7ec7b51c
Larissa Cristina Benedito <larih171099@gmail.com>
2022-09-16 19:43:36 -0300

    compare two strings

---
+++ src/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: larcrist <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/11 03:03:42 by larcrist          #+#    #+#             */
+/*   Updated: 2022/09/11 03:12:34 by larcrist         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	index;
+
+	index = 0;
+	while ((!(s1[index] == '\0' && s2[index] == '\0') && index < n))
+	{
+		if (s1[index] != s2[index])
+		{
+			return (s1[index] - s2[index]);
+		}
+		index++;
+	}
+	return (0);
+}
