c1de19628958b6769035d4f2200435470ee86799
Larissa Cristina Benedito <larih171099@gmail.com>
2022-09-20 22:27:06 -0300

    upp 1/2 + gitignore

--- src/libft.h
+++ src/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <unistd.h> // write
+# include <stdio.h>
 # include <stdlib.h> // malloc(), free() and size_t
 
 /* -------◊	MANIPULATE CHARACTERS	◊------- */
@@ -27,15 +28,35 @@ int	ft_toupper(int c);
 
 /* -------◊	MANIPULATE STRINGS	◊------- */
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+char *ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s1); //mallocagem
+
+
 /* -------◊	FUNCTIONS FOR NUMBERS	◊------- */
+int ft_atoi(const char *nptr);
+
+
+/* -------◊	MANIPULATE MEMORY	◊------- */
+void *memset(void *str, int c, size_t n);
+void ft_bzero(void *s, size_t n);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+void *ft_calloc(size_t nitems, size_t size);
+
+
 
 /* -------◊	    FILE DESCRIPTOR     ◊------- */
 
-/* -------◊	MANIPULATE MEMORY	◊------- */
-
-/* -------◊	MAYBE BÔNUS	◊------- */
+/* -------◊	MAYBE BÔNUS [? lists]	◊------- */
 
 /* -------◊	TO BE CONTINUE	◊------- */
 #endif
