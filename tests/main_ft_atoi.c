da3c5608e39c01a6f0b5311e7d65cffa3870cae4
Larissa Cristina Benedito <larih171099@gmail.com>
2022-09-24 22:47:21 -0300

    main do atoi

---
+++ tests/main_ft_atoi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+static int ft_isspace(const char c)
+{
+    if (c == ' ' || c == '\n' || c == '\t' || c == '\f' || c == '\v' || c == '\r')
+        return (1);
+    return (0);
+}
+int ft_atoi(const char *nptr)
+{
+    int	index;
+    int symbol;
+    int result;
+
+	index = 0;
+	symbol = 1;
+	result = 0;
+    while(ft_isspace(nptr[index]))
+        index++;
+    if(nptr[index] == '+' || nptr[index] == '-')
+    {
+        if(nptr[index] == '-')
+            symbol = symbol * -1;
+        index++;
+    }
+    while(nptr[index] >= '0' && nptr[index] <= '9')
+    {
+        result = (result * 10) + (nptr[index] - 48);
+        index++;
+    }
+    return (symbol * result);
+}
+
+int main(void)
+{
+    printf("%d", ft_atoi("24"));
+    return(0);
+}
