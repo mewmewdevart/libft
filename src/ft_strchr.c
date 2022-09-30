f05adde024b850a75428ae50da2d9c17e41d4ca2
Larissa <larih171099@gmai.com>
2022-09-29 21:24:21 -0300

    checked by test

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -10,17 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0')
+	while (*s != '\0' && *s != (unsigned char)c)
 	{
-		if (*s == (unsigned char) c)
-		{
-			return ((char *)s);
-		}
 		s++;
+	}
+	if (*s == (unsigned char) c)
+	{
+		return ((char *)s);
 	}
 	return (NULL);
 }
