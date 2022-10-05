0525e0abcc3ed71bc60503a1a886ef0506aefa7a
Larissa Cristina Benedito <larih171099@gmail.com>
2022-10-05 20:28:09 -0300

    update in header, make and libft

--- src/libft.h
+++ src/libft.h
@@ -34,19 +34,19 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 
-char		*ft_strdup(const char *s1); //mallocagem
+//char		*ft_strdup(const char *s1); //mallocagem
 /* -------◊	CONVERSION/STRINGS FOR INTEGERS	◊------- */
 int			ft_atoi(const char *nptr);
 
 /* -------◊	MANIPULATE MEMORY	◊------- */
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		ft_bzero(void *s, size_t n);
-
-void		*memset(void *str, int c, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *str, int c, size_t len);
+
+//void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memchr(const void *s, int c, size_t n);
-void		*ft_calloc(size_t nitems, size_t size);
+//void		*ft_calloc(size_t nitems, size_t size);
 
 /* -------◊	    FILE DESCRIPTOR     ◊------- */
 /* -------◊	MAYBE BÔNUS [? lists]	◊------- */
