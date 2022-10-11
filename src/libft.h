411a0ebb2a57aaea5e1baf8cbc2e9ce8b40dcc4f
Larissa Cristina Benedito <larih171099@gmail.com>
2022-10-10 21:28:50 -0300

    fd

--- src/libft.h
+++ src/libft.h
@@ -55,10 +55,10 @@ void		*ft_calloc(size_t nitems, size_t size);
 char		*ft_strdup(const char *s1);
 
 /* -------◊		FUNCTIONS FILE DESCRIPTOR     ◊------- */
-//void ft_putchar_fd(char c, int fd); write
-//void ft_putstr_fd(char *s, int fd); write
-//void ft_putendl_fd(char *s, int fd); write
-//void ft_putnbr_fd(int n, int fd); write
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 /* -------◊	MAYBE BÔNUS [? lists]	◊------- */
 /* -------◊	TO BE CONTINUE	◊------- */
 #endif
