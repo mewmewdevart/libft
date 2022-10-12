38a9920f9bee8289e988dede2769e771fe46561c
Larissa Cristina Benedito <50052600+mewmewdevart@users.noreply.github.com>
2022-10-12 07:52:54 -0300

    fix

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -17,13 +17,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	index;
 
 	index = 0;
-	while ((!(s1[index] == '\0' && s2[index] == '\0') && index < n))
+	if (n == 0)
 	{
-		if (s1[index] != s2[index])
-		{
-			return (s1[index] - s2[index]);
-		}
+		return (0);
+	}
+	while ((s1[index] != '\0' && s2[index] != '\0')
+		&& s1[index] == s2[index] && index < n - 1)
+	{
 		index++;
 	}
-	return (0);
+	return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 }
