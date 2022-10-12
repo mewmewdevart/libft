c72d33a235a094008fd8d73107085357613bfc21
Larissa Cristina Benedito <larih171099@gmail.com>
2022-10-12 18:56:59 -0300

    small changes

--- src/libft.h
+++ src/libft.h
@@ -33,8 +33,8 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
-//char *ft_substr(char const *s, unsigned int start, size_t len); malloca
-//char *ft_strjoin(char const *s1, char const *s2); malloca
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
 //char *ft_strtrim(char const *s1, char const *set); malloca
 //char **ft_split(char const *s, char c); malloca
 //char *ft_strmapi(char const *s, char (*f)(unsigned int, char)); malloca
