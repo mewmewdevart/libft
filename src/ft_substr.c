44fb07e1b5387dd653953e7693e7d050c10279c5
Larissa Cristina Benedito <larih171099@gmail.com>
2022-10-23 17:03:33 -0300

    just update

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -17,22 +17,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 
 	if (!s)
-	{
 		return (NULL);
-	}
 	if (ft_strlen(s) < start)
-	{
+    {
 		len = 0;
 	}
 	if (ft_strlen(s + start) < len)
-	{
 		len = ft_strlen(s + start);
-	}
 	str = (char *)malloc(len + 1);
 	if (!str)
-	{
 		return (NULL);
-	}
 	ft_strlcpy(str, s + start, len + 1);
 	return (str);
 }
